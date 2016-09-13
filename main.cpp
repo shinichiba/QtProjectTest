573be5c702f4b10319ea2bce5054b6345a87a740
dinhba3axiscnc <buidinhba5@gmail.com>
2016-09-14 00:49:20 +0900

    full

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    return a.exec();
+// Add comments and test
+    // Add comments and test
+    // Add comments and test
+    return a.exec();fsdafsdafgsdag
 }
